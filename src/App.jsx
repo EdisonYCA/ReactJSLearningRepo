8ce4a3d3210538ea1549da025d62f41b68cc00d5
Edison Caceres <edisonyca127@gmail.com>
2024-06-08 23:00:00 -0400

    Refactored components to be more organized. Need to fix CoreConcepets component not rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
-import {useState} from "react";
 import Header from "./components/Header/Header.jsx";
-import CoreConcepts from "./components/CoreConcepts.jsx";
-import TabButton  from "./components/TabButton.jsx";
-import { CORE_CONCEPTS } from "./data";
+import CoreConcepts from "./components/CoreConcepts/CoreConcept.jsx";
+import Examples from "./components/Examples/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button!");
-  function selected(component){
-    setSelectedTopic(component);
-  }
-
   return (
-    <div>
-      <Header/>
+    <>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <CoreConcepts />
+        <Examples />
       </main>
-      <section id="core-concepts">
-        <ul>
-          <CoreConcepts {...CORE_CONCEPTS[0]}/>
-          <CoreConcepts {...CORE_CONCEPTS[1]}/>
-          <CoreConcepts {...CORE_CONCEPTS[2]}/>
-          <CoreConcepts {...CORE_CONCEPTS[3]}/>
-        </ul>
-      </section>
-      <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-            <TabButton onSelect={() => selected("components")}>Components</TabButton>
-            <TabButton onSelect={() => selected("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => selected("props")}>Props</TabButton>
-            <TabButton onSelect={() => selected("states")}>States</TabButton>
-        </menu>
-        {selectedTopic}
-      </section>
-    </div>
+    </>
   );
 }
 
