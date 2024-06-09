ec882c8c3621c4463b877b02be41afcccc3bde91
Edison Caceres <edisonyca127@gmail.com>
2024-06-09 12:39:23 -0400

    Fixed issue with core concepts not rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header.jsx";
-import CoreConcepts from "./components/CoreConcepts/CoreConcept.jsx";
+import CoreConcepts from "./components/CoreConcepts/CoreConcepts.jsx";
 import Examples from "./components/Examples/Examples.jsx";
 
 function App() {
